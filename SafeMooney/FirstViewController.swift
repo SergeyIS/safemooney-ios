4d2c315fd25b6e29ea34c0ab5fc63696eb753a5d
nikandv <nikandrov.ant@ya.ru>
2018-03-12 13:58:20 +0300

    init commit

---
+++ SafeMooney/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SafeMooney
+//
+//  Created by Антон Никандров on 12.03.2018.
+//  Copyright © 2018 Anton Nikandrov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SafeMooney/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SafeMooney
+//
+//  Created by Антон Никандров on 12.03.2018.
+//  Copyright © 2018 Anton Nikandrov. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
